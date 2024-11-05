7396e2e8f5ad77ebe4c4263023f4f9caaa8017a2
bettyje <bettyjelagat156@gmail.com>
2024-11-05 21:56:10 +0300

    Modified and addded files

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,39 @@
-import React from "react";
-import Nav from "./Nav";
-
-import hogs from "../porkers_data";
+import React, { useState } from 'react';
+import hogs from '../porkers_data';
+import Nav from './Nav';
+import HogList from './HogList';
+import FilterSortControls from './FilterSortControls';
 
 function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
+  const [filterGreased, setFilterGreased] = useState(false);
+  const [sortOption, setSortOption] = useState('');
+
+  const handleFilterChange = (greased) => {
+    setFilterGreased(greased);
+  };
+
+  const handleSortChange = (option) => {
+    setSortOption(option);
+  };
+
+  const filteredHogs = filterGreased ? hogs.filter(hog => hog.greased) : hogs;
+
+  const sortedHogs = [...filteredHogs].sort((a, b) => {
+    if (sortOption === 'name') {
+      return a.name.localeCompare(b.name);
+    } else if (sortOption === 'weight') {
+      return a.weight - b.weight;
+    }
+    return 0;
+  });
+
+  return (
+    <div className="ui grid container">
+      <Nav />
+      <FilterSortControls onFilterChange={handleFilterChange} onSortChange={handleSortChange} />
+      <HogList hogs={sortedHogs} />
+    </div>
+  );
 }
 
 export default App;
